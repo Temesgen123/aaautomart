1d510762c59a68bd05bd0e86eec81d02f5caca67
Temesgen Birechis <birechis@gmail.com>
2024-11-12 12:15:14 +0300

    File edited

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../../../assets/images/aiheroimage0001.png'
+import img from '/aaautomart/sr/assets/images/aiheroimage0001.png'
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
